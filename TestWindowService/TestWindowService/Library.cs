097b884af35e50b0238e60de46fc3b8b4fdcd3a8
Kinjal G. Shah <kinjal.gshah@EIC.EINFOCHIPS.COM>
2016-12-07 13:45:29 +0530

    first commit

---
+++ TestWindowService/TestWindowService/Library.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TestWindowService
+{
+    public static class Library
+    {
+        public static void WriteErrorLog(Exception ex)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + ex.Source.ToString().Trim() + "; " + ex.Message.ToString().Trim());
+                sw.Flush();
+                int k = 5 / 0;
+                sw.Close();
+            }
+            catch
+            {
+            }
+        }
+
+        public static void WriteErrorLog(string Message)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + Message);
+                sw.Flush();
+                sw.Close();
+                int k = 5 / 0;
+            }
+            catch
+            {
+            }
+        }
+    }
+}
